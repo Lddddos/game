17422ea4eba73b292a2f6b1f561adba39a1c0f78
Lddddos <leonid.ramen@gmaie.com>
2024-12-03 19:57:12 +0100

    add game

---
+++ game.js
@@ -0,0 +1,7 @@
+var random = Math.floor(Math.random() * 10) + 1;
+var pobeda = Math.floor(Math.random() * 10) + 1;
+if(random === pobeda) {
+    console.log("УООООООООООООУ")
+} else {
+    console.log("АХАХХАХАХАХАХААХАХАХ ПР ОСТОФИЛЯ")
+}
